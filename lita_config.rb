5f178bdea7e8d3b77b977b6bf44b92ce44f64133
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-11-22 15:44:44 +0900

    :gem: add handler lita-trello

--- lita_config.rb
+++ lita_config.rb
@@ -3,4 +3,7 @@ Lita.configure do |config|
   config.robot.log_level = :info
   config.robot.adapter = :slack
   config.adapters.slack.token = ENV['SLACK_TOKEN']
+  config.handlers.trello.public_key = ENV['TRELLO_API_KEY']
+  config.handlers.trello.token = ENV['TRELLO_API_TOKEN']
+  config.handlers.trello.board = ENV['TRELLO_BOARD_ID']
 end
