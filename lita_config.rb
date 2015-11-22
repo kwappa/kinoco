8390a1fbd97a2b1e7e1a5cf676fbd283e6a0112c
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-11-22 15:08:45 +0900

    :pencil: modify bot name

--- lita_config.rb
+++ lita_config.rb
@@ -1,5 +1,5 @@
 Lita.configure do |config|
-  config.robot.name = 'Lita'
+  config.robot.name = 'kinoco'
   config.robot.log_level = :info
   config.robot.adapter = :slack
   config.adapters.slack.token = ENV['SLACK_TOKEN']
